3d7641fdd1edcb2036a9fa6e5bc460f6260d6e7d
palmersola <palmersola@yahoo.com>
2022-07-27 14:52:41 -0500

    heroku push

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const api_routes = require("./routes/api_routes");
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
-// app.get("/*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "public/index.html"))
-// );
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
